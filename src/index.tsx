5ff8fb1d541174806b531b3dcd287182c656f07b
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-04-14 09:33:25 +0300

    TASK-2 Added material

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import App from "./components/App";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
