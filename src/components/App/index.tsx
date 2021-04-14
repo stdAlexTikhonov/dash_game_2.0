6fc237a01f540e86daf45f744222ec79feca22ea
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-04-14 09:53:42 +0300

    TASK-3 Press start animation

---
+++ src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div style={{ display: "flex", height: "100vh" }}>
+      <h1 className="text">Press to start</h1>
+    </div>
+  );
+}
+
+export default App;
