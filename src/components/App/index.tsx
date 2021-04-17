27b4694fdd70a03f92fefc4c2cf53993830b9362
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-04-17 13:54:44 +0300

    feat(DevTools)

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,12 @@
 import { Routes } from "../../routes";
 import Box from "@material-ui/core/Box";
 import AppBar from "../AppBar";
+import DevTools from "mobx-react-devtools";
 
 const App = () => (
   <Box height="100vh" width="100%" display="flex" bgcolor="black">
+    <DevTools />
     <AppBar />
-
     <Routes />
   </Box>
 );
