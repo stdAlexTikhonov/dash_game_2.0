4f189a656e5a4d1084820448df02fe70d2ef0e7c
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-04-18 22:05:46 +0300

    TASK-11 test draw pic on canvas

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,26 +1,44 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
+import ground from "../../assets/images/ground.png";
 
 export const Canvas = () => {
-  const canvasRef = useRef(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
   const requestIdRef = useRef(null as null | number);
+  const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
+
+  const image = new Image();
 
   const renderFrame = () => {
-    // ...
+    context!.drawImage(image, 0, 0, 32, 32);
   };
 
   const tick = () => {
-    if (!canvasRef.current) return;
+    if (!context) return;
+
     renderFrame();
-    // requestAnimationFrame(tick);
-    requestIdRef.current = requestAnimationFrame(tick);
+    // requestIdRef.current = requestAnimationFrame(tick);
   };
 
   useEffect(() => {
-    requestIdRef.current = requestAnimationFrame(tick);
+    if (canvasRef.current)
+      setContext((_) => canvasRef.current!.getContext("2d"));
+
     return () => {
       cancelAnimationFrame(requestIdRef.current!);
     };
   }, []);
 
-  return <canvas width={window.innerWidth} ref={canvasRef} />;
+  useEffect(() => {
+    image.src = ground;
+    requestAnimationFrame(tick);
+  }, [context]);
+
+  return (
+    <canvas
+      width={window.innerWidth}
+      height={document.body.clientHeight}
+      ref={canvasRef}
+    />
+  );
 };
