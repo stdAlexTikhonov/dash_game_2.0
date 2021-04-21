e18389c2d8ca51ac591ee3c4eecf5b6f8ed18602
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-04-21 21:56:08 +0300

    game reset

--- src/components/StartScreen/index.tsx
+++ src/components/StartScreen/index.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useStyles } from "./styles";
+import GameStore from "../../store/GameStore";
+import Levels from "../../levels";
 import "./styles.css";
 
 export const StartScreen = () => {
+  useEffect(() => {
+    GameStore.setLevelMap(Levels[GameStore.level]);
+  }, []);
   const classes = useStyles();
   return (
     <Link to="/game" className={classes.press_start}>
