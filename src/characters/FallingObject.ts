e215004a02d88262f45b20bb890a7eced794fdf1
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-06-08 00:17:55 +0300

    TASK-30 Fix Out of map

--- src/characters/FallingObject.ts
+++ src/characters/FallingObject.ts
@@ -14,20 +14,22 @@ export default class FallingObject extends GameObject {
 
   check_way_right() {
     const { world_map } = World;
-    return (
-      world_map[this.y][this.x + 1] === " " &&
-      world_map[this.y + 1][this.x + 1] === " " &&
-      !["O", "*"].includes(world_map[this.y - 1][this.x + 1])
-    );
+    return world_map[this.y - 1]
+      ? world_map[this.y][this.x + 1] === " " &&
+          world_map[this.y + 1][this.x + 1] === " " &&
+          !["O", "*"].includes(world_map[this.y - 1][this.x + 1])
+      : world_map[this.y][this.x + 1] === " " &&
+          world_map[this.y + 1][this.x + 1] === " ";
   }
 
   check_way_left() {
     const { world_map } = World;
-    return (
-      world_map[this.y][this.x - 1] === " " &&
-      world_map[this.y + 1][this.x - 1] === " " &&
-      !["O", "*"].includes(world_map[this.y - 1][this.x - 1])
-    );
+    return world_map[this.y - 1]
+      ? world_map[this.y][this.x - 1] === " " &&
+          world_map[this.y + 1][this.x - 1] === " " &&
+          !["O", "*"].includes(world_map[this.y - 1][this.x - 1])
+      : world_map[this.y][this.x - 1] === " " &&
+          world_map[this.y + 1][this.x - 1] === " ";
   }
 
   move_possible() {
