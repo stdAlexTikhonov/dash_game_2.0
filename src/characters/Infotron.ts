25ad33e9a290c4e5c63a6188be418f6ee37e70d3
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-06-15 04:22:41 +0300

    TASK-34 Added infotron asset

--- src/characters/Infotron.ts
+++ src/characters/Infotron.ts
@@ -1,5 +1,7 @@
 import FallingObject from "./FallingObject";
 import food from "../assets/images/food.png";
+import World from "./World";
+import { BLOCK_WIDTH } from "../utils/constansts";
 
 export default class Infotron extends FallingObject {
   img: HTMLImageElement = new Image();
@@ -7,5 +9,25 @@ export default class Infotron extends FallingObject {
   constructor(y: number, x: number) {
     super(y, x, "*");
     this.img.src = food;
+  }
+
+  draw(
+    context: CanvasRenderingContext2D,
+    direction: string,
+    animation: boolean,
+    viewport_start_y: number,
+    viewport_start_x: number,
+    value: number
+  ) {
+    super.draw(
+      context,
+      direction,
+      animation,
+      viewport_start_y,
+      viewport_start_x,
+      value,
+      this.left || this.right ? World.counter % 8 : 0,
+      this.left ? BLOCK_WIDTH : 0
+    );
   }
 }
