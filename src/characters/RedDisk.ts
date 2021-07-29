953a494a9f598406808bcffee433036ad48d9169
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-07-29 15:58:44 +0300

    RedDisc animation

--- src/characters/RedDisk.ts
+++ src/characters/RedDisk.ts
@@ -1,5 +1,6 @@
 import Bomb from "./Bomb";
 import red_disk from "../assets/images/red_disk.png";
+import { BLOCK_WIDTH } from "../utils/constansts";
 export default class RedDisc extends Bomb {
   img: HTMLImageElement = new Image();
   activated: boolean = false;
@@ -20,5 +21,24 @@ export default class RedDisc extends Bomb {
     super.updateState();
     if (this.activated) this.count--;
     if (this.count <= 0) this.detonate();
+  }
+
+  draw(
+    context: CanvasRenderingContext2D,
+    direction: string,
+    animation: boolean,
+    viewport_start_y: number,
+    viewport_start_x: number,
+    value: number
+  ) {
+    super.draw(
+      context,
+      direction,
+      animation,
+      viewport_start_y,
+      viewport_start_x,
+      value,
+      this.count % 2 === 0 ? 0 : BLOCK_WIDTH
+    );
   }
 }
