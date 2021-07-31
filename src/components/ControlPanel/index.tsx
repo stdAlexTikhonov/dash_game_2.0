3cf6936fc9f258500cf022a7ea404e2cc2279fe3
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-08-01 00:53:39 +0300

    TASK-56 Base control panel UI

---
+++ src/components/ControlPanel/index.tsx
@@ -0,0 +1,60 @@
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import image from "../../assets/images/infotron.png";
+import bomb from "../../assets/images/bomb.png";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    text: {
+      fontSize: 20,
+      color: "red",
+      marginLeft: 10,
+    },
+    block: {
+      background: "black",
+      padding: 5,
+      width: 90,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  })
+);
+
+export const ControlPanel = () => {
+  const classes = useStyles();
+
+  return (
+    <Box
+      width="100%"
+      position="absolute"
+      bottom="0px"
+      display="flex"
+      justifyContent="flex-end"
+    >
+      <Box
+        flexGrow={1}
+        justifyContent="center"
+        alignItems="center"
+        display="flex"
+      >
+        <Typography variant="h5" className={classes.text}>
+          00:00
+        </Typography>
+      </Box>
+      <Box className={classes.block}>
+        <img src={image} alt="infotron" width={18} height={18} />
+        <Typography variant="h5" className={classes.text}>
+          00/00
+        </Typography>
+      </Box>
+      <Box className={classes.block}>
+        <img src={bomb} alt="bombs" width={18} height={18} />
+        <Typography variant="h5" className={classes.text}>
+          0
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
