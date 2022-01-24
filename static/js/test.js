9423af84527b4855066ffedbfabb42213b95e4a5
Alexander <stdAlexTikhonov@hotmail.com>
2022-01-24 21:21:17 +0300

    save

--- static/js/test.js
+++ static/js/test.js
@@ -1,13 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("hello");
-  let elem = document.documentElement;
-
-  elem
-    .requestFullscreen({ navigationUI: "hide" })
-    .then(() => {})
-    .catch((err) => {
-      alert(
-        `An error occurred while trying to switch into full-screen mode: ${err.message} (${err.name})`
-      );
-    });
-});
+document.addEventListener(
+  "keydown",
+  function (e) {
+    if (e.key === "Enter") {
+      elem
+        .requestFullscreen({ navigationUI: "hide" })
+        .then(() => {})
+        .catch((err) => {
+          alert(
+            `An error occurred while trying to switch into full-screen mode: ${err.message} (${err.name})`
+          );
+        });
+    }
+  },
+  false
+);
